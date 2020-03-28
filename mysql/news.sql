998c8d5485fe9ef687a50166cc98732298818523
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-03-28 17:18:03 -0300

    modelagem banco de dados

---
+++ mysql/news.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS news_api;
+USE news_api;
+
+CREATE TABLE news (
+  `idnews` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `fonte` VARCHAR(255) NULL,
+  `title` TEXT(255) NULL,
+  `description` TEXT(255) NULL,
+  `url_to_link` VARCHAR(255) NULL,
+  `url_to_image` VARCHAR(255) NULL,
+  `published_at` VARCHAR(45) NULL
+) ENGINE = InnoDB;
