ab940e8276e0305ffe3e3197ff3357b4b4154249
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-03-28 17:48:15 -0300

    connection mysql, env e gitignore

---
+++ mysql/news.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : 'news_api'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
