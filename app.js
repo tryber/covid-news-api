9abb15c90f15da54936c5c7bde66b9122f95a259
vrramosm <vf.ramos7@gmail.com>
2020-03-28 22:20:11 -0300

    Adicionado a opçao de se conectar a porta 4000!

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.get('/', (req, res) => {
   connection.end();
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!');
 });
