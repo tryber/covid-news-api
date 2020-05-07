b4325bfe1580fbaf9292ecc738da401841a7b5dc
Cairo Noleto <caironoleto@gmail.com>
2020-05-07 18:02:13 -0300

    remove unique index from fonte and title columns. Isn't necessary.

--- mysql/news.sql
+++ mysql/news.sql
@@ -3,8 +3,8 @@ USE news_api;
 
 CREATE TABLE news (
   `idnews` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `fonte` VARCHAR(255) NULL UNIQUE,
-  `title` VARCHAR(255) NULL UNIQUE,
+  `fonte` VARCHAR(255) NULL,
+  `title` VARCHAR(255) NULL,
   `description` TEXT NULL,
   `url_to_link` VARCHAR(255) NULL,
   `url_to_image` TEXT NULL,
